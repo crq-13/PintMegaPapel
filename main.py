d511f5f700c512f2633949dac0c29dc9a01cb7e5
Cristian Rojas <cristian@sioma.app>
2021-08-11 09:43:21 -0500

    DialogWindow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from FirstTest import *
+from DialogMail import *
 import qpageview
 from PyQt5 import QtWebEngineWidgets
 from PyQt5 import QtNetwork
@@ -15,10 +16,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
         # Conectamos los eventos con sus acciones
-        self.pushButton.clicked.connect(self.actualizar)
+        self.pushButton.clicked.connect(self.onMailClick)
 
-    def actualizar(self):
-        self.label.setText("¡Acabas de hacer clic en el botón!")
+    def onMailClick(self):
+        dlg = DialogMail(self)
+        dlg.exec()
+
+class DialogMail(QtWidgets.QDialog):
+    def __init__(self, parent=None, *args, **kwargs):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.lineEdit.returnPressed.connect(self.ui.lineEdit.clear)
+
+    #def onInputBoxClick(self):
+
 
 
 
